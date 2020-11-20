ce54ccdb63c76237416f616ae1416cfbfe418722
Chris Werkheiser <cjwerk@gmail.com>
2020-11-20 12:54:39 -0800

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,76 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { type } = require('os');
 
-];
+
+// array of questions for user
+function promptUser(){
+    return inquirer.prompt([
+        
+        
+            {
+                type: "input",
+                name: "projectTitle",
+                message: "What is the project title?",
+            },
+            {
+                type: "input",
+                name: "description",
+                message: "Write a brief description of your project: "
+            },
+            
+            {
+                type: 'input',
+                name: 'Installation',
+                message: 'Describe the installation process:'
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: "What is this project usage for?"
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Choose an appropriate license for this project:',
+                choices: [
+                    "Apache",
+                    "Academic",
+                    "GNU",
+                    "ISC",
+                    "MIT",
+                    "Mozilla",
+                    "Open"
+                ]
+            },
+            {
+                type: 'input',
+                name: 'contributor',
+                message: 'Who are the contributors to this project?'
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Is there a test included?'
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: 'What do I do if i have an issue?'
+            },
+            {
+                type: 'input',
+                name: 'username',
+                message: 'What is your Github username?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter your email address?'
+            }
+        
+    ]);
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
